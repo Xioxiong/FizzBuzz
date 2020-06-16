8b3f20efbe9e6cdf17a3b6e02a367c995c7a7c9a
xiongwei <2894001817@qq.com>
2020-06-16 11:51:56 +0800

    修改move的方法，增加初始化为E方向的方法

--- empty-repo/src/main/java/com.thoughtworks/Execution.java
+++ empty-repo/src/main/java/com.thoughtworks/Execution.java
@@ -1,42 +1,53 @@
 package com.thoughtworks;
 
 public class Execution {
-    int x = 0;
-    int y = 0;
-    StringBuilder result = new StringBuilder();
+
 
     public String move(String command,String direction) {
+        int x = 0;
+        int y = 0;
+        StringBuilder result = new StringBuilder();
+        String ort = direction;
+
         if("N".equals(direction)){
             if("M".equals(command)){
                 y +=1;
             }else if("L".equals(command)){
-                direction = "W";
+                ort = "W";
             }else{
-                direction = "E";
+                ort = "E";
             }
         }
         if("S".equals(direction)){
             if("M".equals(command)){
                 y -= 1;
             }else if("L".equals(command)){
-                direction = "E";
+                ort = "E";
             }else {
-                direction = "W";
+                ort = "W";
             }
         }
         if("W".equals(direction)){
             if ("M".equals(command)) {
                 x -=1;
             }else if("L".equals(command)){
-                direction = "S";
+                ort = "S";
             }else {
-                direction = "N";
+                ort = "N";
             }
         }
-
+        if("E".equals(direction)){
+            if ("M".equals(command)) {
+                x +=1;
+            }else if("L".equals(command)){
+                ort = "N";
+            }else {
+                ort = "S";
+            }
+        }
         result.append(x);
         result.append(y);
-        result.append(direction);
+        result.append(ort);
         return result.toString();
     }
 
